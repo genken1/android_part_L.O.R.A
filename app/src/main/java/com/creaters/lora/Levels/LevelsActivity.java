89fafea72f4f7fb1576af2205467f65000e92122
oleg <chushckin.ol@yandex.ru>
2020-05-26 14:49:44 +0300

    unity library

--- app/src/main/java/com/creaters/lora/Levels/LevelsActivity.java
+++ app/src/main/java/com/creaters/lora/Levels/LevelsActivity.java
@@ -2,6 +2,7 @@ package com.creaters.lora.Levels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -11,11 +12,13 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.creaters.lora.R;
+import com.unity3d.player.UnityPlayerActivity;
 
 public class LevelsActivity extends AppCompatActivity {
     private ImageView level;
     private LevelPresenter presenter;
     private LevelsModel model;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +27,14 @@ public class LevelsActivity extends AppCompatActivity {
         level = (ImageView) findViewById(R.id.level);
         presenter = new LevelPresenter(this);
         model = new LevelsModel(this);
-
+        intent = new Intent(this, UnityPlayerActivity.class);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         level.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivity(intent);
                 Toast.makeText(getApplicationContext(), "Start level", Toast.LENGTH_SHORT).show();
                 model.loadAchievements();
             }
